8c790894acb3aca4f61c24d520047a90088bf52e
Aaron Greenspan <afg419@gmail.com>
2015-11-25 11:28:14 -0700

    feat: started command line parser

---
+++ lib/enigma/cli.ex
@@ -0,0 +1,27 @@
+defmodule Enigma.CLI do
+
+  @moduledoc """
+    Handle the command line parsing...
+  """
+
+  # def run(argv) do
+  #   parse_args(argv)
+  # end
+
+  @doc """
+    argv can be -e for encrypt
+    or -d for decrypt
+    or -c for crack
+  """
+
+  # def parse_args(argv) do
+  #   parse = OptionParser.parse(argv, switches: [help: :boolean, c: :crack, d: :decrypt, e: :encrypt],
+  #                                    aliases: [h: :help])
+  #   case parse do
+  #     {}
+  #
+  #
+  #   end
+  # end
+
+end
